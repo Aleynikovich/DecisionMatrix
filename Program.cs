47f351f523778fd8a9e15492cb90d0023cdc2fe3
Alexander Kalis <alex.kalis@outlook.com>
2021-05-04 22:35:16 +0200

    IndustrialOptimization

--- Program.cs
+++ Program.cs
@@ -6,7 +6,6 @@ namespace DecisionMatrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Simulador de juegos suma cero. Creado por Alexander Kalis");
             StrategicDominance(4);
         }
 
@@ -14,7 +13,7 @@ namespace DecisionMatrix
         {
             int[,] stratMatrix = new int[Strategies, Strategies];
             bool correctNumber;
-            bool unviable;
+            bool[] stratUnviable = new bool[2];
             Console.WriteLine("Starting Strategic Dominance for " + Strategies + "strategies \n");
 
             //Input data
@@ -34,6 +33,7 @@ namespace DecisionMatrix
                 }
             }
 
+            Console.Clear();
             //Check estrategias inviables para jugador 2
             for (int i = 0; i < Strategies - 1; i++) //strat principal
             {
@@ -41,55 +41,70 @@ namespace DecisionMatrix
                 {
                     if (i < j)
                     {
-                        unviable = false;
+                        stratUnviable[0] = false;
+                        stratUnviable[1] = false;
                         //Console.WriteLine("Comparando estrategia {0} con estrategia {1}", i + 1, j + 1);
                         for (int k = 0; k < Strategies; k++) //Filas
                         {
                             //Console.WriteLine("Comparando {0} con {1}", stratMatrix[i, k], stratMatrix[j, k]);
-                            if (stratMatrix[i, k] > stratMatrix[j, k])
+                            if (stratMatrix[i, k] > stratMatrix[j, k] && !stratUnviable[0])
                             {
-                                Console.WriteLine("Estrategia {0} no siempre es mejor que estrategia {1} para jugador 2", i + 1, j + 1);
-                                unviable = true;
-                                break;
+                                stratUnviable[0] = true;
+                            }
+                            if (stratMatrix[i, k] < stratMatrix[j, k] && !stratUnviable[1])
+                            {
+                                stratUnviable[1] = true;
                             }
                         }
 
-                        if (unviable == false)
+                        if (!stratUnviable[0])
                         {
                             Console.WriteLine("Estrategia {0} siempre es mejor que estrategia {1} para jugador 2", i + 1, j + 1);
+                        }
+
+                        if (!stratUnviable[1])
+                        {
+                            Console.WriteLine("Estrategia {1} siempre es mejor que estrategia {0} para jugador 2", i + 1, j + 1);
                         }
                     }
                 }
             }
 
             //Check estrategias inviables para jugador 1
-            for (int i = 0; i < Strategies-1; i++) //strat principal
+            for (int i = 0; i < Strategies - 1; i++) //strat principal
             {
                 for (int j = 1; j < Strategies; j++) //strat secundaria
                 {
                     if (i < j)
                     {
-                        unviable = false;
+                        stratUnviable[0] = false;
+                        stratUnviable[1] = false;
                         //Console.WriteLine("Comparando estrategia {0} con estrategia {1}", i + 1, j + 1);
                         for (int k = 0; k < Strategies; k++) //Filas
                         {
-                            //Console.WriteLine("Comparando {0} con {1}", stratMatrix[k, i], stratMatrix[k, j]);
-                            if (stratMatrix[k, i] < stratMatrix[k, j])
+                            //Console.WriteLine("Comparando {0} con {1}", stratMatrix[i, k], stratMatrix[j, k]);
+                            if (stratMatrix[k, i] < stratMatrix[k, j] && !stratUnviable[0])
                             {
-                                Console.WriteLine("Estrategia {0} no siempre es mejor que estrategia {1} para jugador 1", i + 1, j + 1);
-                                unviable = true;
-                                break;
+                                stratUnviable[0] = true;
+                            }
+                            if (stratMatrix[k, i] > stratMatrix[k, j] && !stratUnviable[1])
+                            {
+                                stratUnviable[1] = true;
                             }
                         }
 
-                        if (unviable == false)
+                        if (!stratUnviable[0])
                         {
                             Console.WriteLine("Estrategia {0} siempre es mejor que estrategia {1} para jugador 1", i + 1, j + 1);
+                        }
+
+                        if (!stratUnviable[1])
+                        {
+                            Console.WriteLine("Estrategia {1} siempre es mejor que estrategia {0} para jugador 1", i + 1, j + 1);
                         }
                     }
                 }
             }
-
         }
     }
 }
